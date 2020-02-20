b0cee1b9a27a34d4b9a5545784b4cf53f35114a1
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-20 10:20:00 -0500

    Almost complete, missing Quest 5

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(array){
+return array[0]
+}
 
+function getLastItemFrom(array){
+  return array.pop()
+}
 
+function getIndex3(array){
+  if (array.length > 4){
+  return array[3]}
+    else{
+      return array.pop()
+    }
+  }
 
+function isLongList(array){
+  if (array.length >= 10){
+    return true
+  }else
+    {
+    return false  
+    }
 
+}
 
+function firstItemIsNumber(array){
+  
+}
 
+function secondCharOfThirdString(array){
+  return array[2][1]
+}
 
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
